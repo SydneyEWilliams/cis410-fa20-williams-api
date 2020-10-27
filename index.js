7ce20ee384a79eb91ff0889e43af2138c358858d
SydneyEWilliams <sydney.williams@colostate.edu>
2020-10-27 17:42:26 -0600

    10.1 10/27 database, /movies

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const db = require('./dbConnectExec.js')
 
 const app = express();
 
@@ -12,6 +13,22 @@ res.send("heloo world")
 // app.put()
 // app.delete
 
+
+app.get("/movies", (req,res)=>{
+    //get data from database
+    db.executeQuery(`SELECT * FROM movie
+    LEFT JOIN Genre
+    ON genre.GenrePK = movie.GenreFK`)
+
+    .then((result)=>{
+        res.status(200).send(result)
+    })
+
+    .catch((error)=>{
+        console.log(error)
+        res.status(500).send()
+    })
+})
 
 app.listen(5000,()=>{
     console.log("App runnin")
