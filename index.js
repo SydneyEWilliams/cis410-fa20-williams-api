ee9aac5da4f6582f5b3a3ef3b01e9a6459f68e9e
SydneyEWilliams <sydney.williams@colostate.edu>
2020-10-27 18:03:13 -0600

    10.1 10/27 /movies/:pk, having issues

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.get("/hi",(req,res)=>{
-res.send("heloo world")
+res.send("hello world")
 })
 
 
@@ -30,6 +30,35 @@ app.get("/movies", (req,res)=>{
     })
 })
 
+app.get("/movies/:pk", (req,res)=>{
+    var pk = req.params.pk
+    //console.log("my pk:" + pk)
+
+    var myQuery =`SELECT * FROM movie
+    LEFT JOIN Genre
+    ON genre.GenrePK = movie.GenreFK
+    WHERE moviePK = ${pk}`
+
+    db.executeQuery(myQuery)
+        .then((movies)=>{
+            //console.log("Movies: ", movies
+            
+            if(movies[0]){
+                res.send(movies[0])
+            }
+
+            else{
+                res.status(404).send("bad request")
+            }
+        })
+
+        .catch((error)=>{
+            console.log("Error in /movies/pk", error)
+            res.status(500).send()
+        })
+})
+
+
 app.listen(5000,()=>{
-    console.log("App runnin")
+    console.log("App runnin on 5000")
 })
